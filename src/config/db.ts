25a3520fac7b9551851514b92dea53c5203730d6
Misael Miranda <misaelbahia1@hotmail.com>
2024-12-11 15:39:57 -0300

    update db 3

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,13 +2,15 @@ import { Pool } from 'pg';
 import { config } from 'dotenv';
 
 config();
-const { PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT } = process.env;
+const { PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT, DATABASE_URL } =
+  process.env;
 const pool = new Pool({
   host: PGHOST,
   user: PGUSER,
   password: PGPASSWORD,
   database: PGDATABASE,
   port: Number(PGPORT),
+  connectionString: DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
