2d73439265a95b11b0d347d74d56e30847881689
misereitor <misaelbahia1@hotmail.com>
2025-01-27 09:19:54 -0300

    update send sms

--- src/services/whatsapp-sms.service.ts
+++ src/services/whatsapp-sms.service.ts
@@ -24,36 +24,37 @@ export async function createCode(user: UserVote) {
 export async function sendMessage(code: string, phone: string) {
   const codeEdit = `${code.substring(0, 3)}-${code.substring(3, 6)}`;
   const message = `Código de validação para a pesquisa Melhores do Ano 2024: ${codeEdit}`;
-  const checkNumberIsWhatsapp = await fetch(
-    `${WHATSAPP_API_URL}/api/contacts/check-exists?phone=${phone}&session=${WHATSAPP_SESSION}`,
+  // const checkNumberIsWhatsapp = await fetch(
+  //   `${WHATSAPP_API_URL}/api/contacts/check-exists?phone=${phone}&session=${WHATSAPP_SESSION}`,
+  //   {
+  //     headers: {
+  //       'X-API-KEY': String(WHATSAPP_X_APY_KEY)
+  //     }
+  //   }
+  // );
+  //if (checkNumberIsWhatsapp.ok) {
+  // const data = await checkNumberIsWhatsapp.json();
+  // console.log(data);
+  // if (data.numberExists) {
+  //   await sendMessageWhatsapp(data.chatId, message);
+  //   return;
+  // }
+  const checkNumberIsWhatsappTwo = await fetch(
+    `${WHATSAPP_API_URL}/api/contacts/check-exists?phone=${'55' + phone}&session=${WHATSAPP_SESSION}`,
     {
       headers: {
         'X-API-KEY': String(WHATSAPP_X_APY_KEY)
       }
     }
   );
-  if (checkNumberIsWhatsapp.ok) {
-    const data = await checkNumberIsWhatsapp.json();
-    if (data.numberExists) {
-      await sendMessageWhatsapp(data.chatId, message);
+  const data2 = await checkNumberIsWhatsappTwo.json();
+  if (checkNumberIsWhatsappTwo.ok) {
+    if (data2.numberExists) {
+      await sendMessageWhatsapp(data2.chatId, message);
       return;
     }
-    const checkNumberIsWhatsappTwo = await fetch(
-      `${WHATSAPP_API_URL}/api/contacts/check-exists?phone=${'55' + phone}&session=${WHATSAPP_SESSION}`,
-      {
-        headers: {
-          'X-API-KEY': String(WHATSAPP_X_APY_KEY)
-        }
-      }
-    );
-    const data2 = await checkNumberIsWhatsappTwo.json();
-    if (checkNumberIsWhatsappTwo.ok) {
-      if (data2.numberExists) {
-        await sendMessageWhatsapp(data2.chatId, message);
-        return;
-      }
-    }
   }
+  //}
   throw new AppError('Número de whastapp invalido!', 404);
 }
 
